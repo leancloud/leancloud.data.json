257775045ba5fe7219e008388d53ea2d5541793a
dennis zhuang <killme2008@gmail.com>
2015-12-28 17:31:32 +0800

    Set compile target to 1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leancloud.data.json "0.1.0-RC2"
+(defproject leancloud.data.json "0.1.0-RC3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :java-source-paths ["src/jvm"]
   :source-paths ["src/clj"]
+  :javac-options ["-target" "1.7" "-source" "1.7"]
   :jvm-opts ["-Xmx1g" "-server" "-XX:MaxPermSize=256m" "-XX:+UseConcMarkSweepGC" "-XX:+UseCMSCompactAtFullCollection" "-Dclojure.compiler.elide-meta='[:doc :added]'"]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [clj-tuple "0.1.6"]
